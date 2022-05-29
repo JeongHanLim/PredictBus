e03f59c7e46dd0713d952951fbcbebac240ba3bb
JeongHanLim <jhlim271@naver.com>
2022-05-29 23:43:20 +0000

    call api fin.

--- fetch_data.py
+++ fetch_data.py
@@ -1,12 +1,15 @@
 
 import requests
 import json
+import xmltodict
+from datetime import datetime
 
 class get_station_data(object):
-    def __init__(self, station_name="신안인스빌", key_file = "key_param.json", link_file = "link.json"):
+    def __init__(self, key_file = "key_param.json", link_file = "link.json"):
         self.encode_key, self.decode_key, self.station_key = self.parse_key(key_file)
         self.arrival_url, self.station_url = self.parse_link(link_file)
-        self.station_id = self.get_station_id(self.station_url, self.station_key)
+        self.station_id = 121000009 #신분당선강남역
+
 
     @staticmethod
     def parse_key(key_file):
@@ -20,23 +23,45 @@ class get_station_data(object):
             link_data = json.load(f)
         return link_data["arrival_url"], link_data["station_url"]
 
-    def get_station_id(self, station_name)
-        params= {}
+    def get_station_id(self, station_name):
+        pass
+        #평택신안인스빌 : 214001274
+        #신분당선강남역 : 121000009
+        #고덕르폴로랑아파트 : 214001182
+        #openapi.gbis.go.kr/ws/rest/busstationservice?serviceKey=1234567890&keyword=고덕르폴로랑아파트
 
-    def something(self):
-        params = {'serviceKey': '서비스키', 'stationId': '200000078'}
-        response = requests.get(self.station_url, params=params)
-
-
+    def realtime_status(self, station_id):
+        #판교도서관:206000004
+        key = self.encode_key
+        url = "http://apis.data.go.kr/6410000/busarrivalservice/getBusArrivalList?serviceKey={}&stationId={}".format(key,station_id)
+        response = requests.get(url).content
+        current_time = str(datetime.now().strftime("%H:%M"))
+        with open("data/"+current_time+"_"+str(station_id)+".txt", "wb") as f:
+           f.write(response)
         return
 
 
 
-response = requests.get(arrival_url, params=params)
-print(response.content)
+# response = requests.get(arrival_url, params=params)
+# print(response.content)
 
 
 
 if __name__ == "__main__":
-    station_key = get_station_name("신안인스빌")
+    # Airflow / Jenkins 
+    # station_key = get_station_name("신안인스빌")
+    import time
+    from threading import Thread
+    TIME_INTERVAL = 5
+
+    getter = get_station_data()
+    while True:
+        if int(time.time())%TIME_INTERVAL == 0:
+            # TODO : Make each one's thread
+            getter.realtime_status(121000009) # 신분당선강남여121000009
+            getter.realtime_status(214001274)
+            getter.realtime_status(214001182)
+            # 구미동차고지 206000335
+            print(str(datetime.now().strftime("%H:%M")) + ", file saved")
+            time.sleep(60)
 
